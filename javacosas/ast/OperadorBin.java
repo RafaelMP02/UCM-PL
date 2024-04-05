7c866d67db39f48c287eda6d844914af244acd95
Antonio Parrilla Sánchez <antonio@MacBook-Air-de-Antonio.local>
2024-04-05 07:21:22 +0200

    a

---
+++ javacosas/ast/OperadorBin.java
@@ -0,0 +1,7 @@
+package ast;
+
+public enum  OperadorBin {
+    BinAccesoArrays, BinConjuncion, BinDisyuncion, BinIgual,
+    BinDistinto, BinMenorIgual, BinMayorIgual, BinMenorQue, BinMayorQue, BinSuma,
+    BinResta, BinMul, BinDiv
+}
---
+++ javacosas/ast/OperadorUn.java
@@ -0,0 +1,5 @@
+package ast;
+
+public enum OperadorUn {
+    UnAccesoCampo, UnAccesoPuntero, UnIncremento, UnDecremento, UnMas, UnMenos
+}
